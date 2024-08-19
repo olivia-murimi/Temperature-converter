b17f2bc9943888d215ae785d39683195d8a4e78d
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-19 23:12:20 +0300

    switch

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-
-
-
+const celsiusEl = document.getElementById("celsius")
+const fahrenheitEl = document.getElementById("fahrenheit")
+const kelvinEl = document.getElementById("kelvin")
 
 function computeTemp(event){
-    console.log(event.target.name);
+    const currentValue = +event.target.value
+
+    switch () {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
 }
